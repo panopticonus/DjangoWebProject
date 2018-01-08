f70db13d0bfa42f0dfa9ba6a4d8b231154fa29e7
Kamil Lang <k_lang@wp.pl>
2018-01-08 01:03:55 +0100

    Add project files.

---
+++ DjangoWebProject/app/views.py
@@ -0,0 +1,33 @@
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from _io import StringIO
+from django.http import HttpRequest
+from datetime import datetime
+from django.http import HttpResponseRedirect
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+	)
+
+def imageTypeConverter(request):
+    if request.method == 'POST':
+        item = request.FILES.get('imgInp')
+        return HttpResponseRedirect('imageTypeConverter')
+    else:
+        assert isinstance(request, HttpRequest)
+        return render(request,
+		    'app/imageTypeConverter.html',
+		    {
+			    'title':'Konwerter plików graficznych',
+                'year':datetime.now().year,
+		    })
